d02892fb68b4cf69758af3f0f3efd54313f30147
oshvartz <offirsh@yahoo.com>
2018-06-07 14:42:53 +0300

    first try

---
+++ OS.ReactiveDuplexChannel/ReactiveDuplexChannelFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using Castle.DynamicProxy;
+
+namespace OS.ReactiveDuplexChannel
+{
+  public class ReactiveDuplexChannelFactory<TContract, TCallbackContract> : DuplexChannelFactory<TContract>
+where TContract : class
+where TCallbackContract : class
+  {
+    private static readonly ProxyGenerator _generator = new ProxyGenerator();
+    private ReactiveInteceptor _reactiveInteceptor;
+
+    public ReactiveDuplexChannelFactory(Binding binding, string remoteAddress)
+      : base(new InstanceContext(_generator.CreateInterfaceProxyWithoutTarget<TCallbackContract>(new ReactiveInteceptor())), binding, remoteAddress)
+    {
+      _reactiveInteceptor = ExtractTheInterceptor();
+    }
+
+
+    //kongfu
+    private ReactiveInteceptor ExtractTheInterceptor()
+    {
+      var cbi = this.GetType().GetProperty("CallbackInstance", BindingFlags.Instance | BindingFlags.NonPublic);
+      var ic = (InstanceContext) cbi.GetValue(this);
+
+      var memb = ic.GetType().GetProperty("UserObject", BindingFlags.Instance | BindingFlags.NonPublic);
+
+      var obk = memb.GetValue(ic);
+
+      var interc = obk.GetType().GetField("__interceptors", BindingFlags.Instance | BindingFlags.NonPublic);
+      var arr = interc.GetValue(obk) as IInterceptor[];
+
+       return (ReactiveInteceptor) arr[0];
+    }
+
+    public IObservable<T> AsObservable<T>()
+    {
+      return _reactiveInteceptor.AddListener<T>();
+    }
+  }
+}
---
+++ OS.ReactiveDuplexChannel/ReactiveInteceptor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reactive.Subjects;
+using System.Reflection;
+using Castle.DynamicProxy;
+
+namespace OS.ReactiveDuplexChannel
+{
+  internal class ReactiveInteceptor : IInterceptor
+  {
+    private readonly ConcurrentDictionary<Type, OnNextInvoker> _subjects = new ConcurrentDictionary<Type, OnNextInvoker>();
+
+    public void Intercept(IInvocation invocation)
+    {
+      if (invocation.Arguments.Length == 1)
+      {
+        if (_subjects.TryGetValue(invocation.Arguments[0].GetType(), out OnNextInvoker onNextInvoker))
+        {
+          onNextInvoker?.Invoke(invocation.Arguments[0]);
+        }
+      }
+      else
+      {
+        //log
+        invocation.Proceed();
+      }
+    }
+
+
+    internal IObservable<T> AddListener<T>()
+    {
+      ValidateTypeInCallback<T>();
+      var subject = new Subject<T>();
+      return (Subject<T>)_subjects.GetOrAdd(typeof(T), OnNextInvoker.Create(subject)).Traget;
+    }
+
+    private void ValidateTypeInCallback<T>()
+    {
+      // throw new NotImplementedException();
+    }
+
+    private class OnNextInvoker
+    {
+      MethodInfo MethodInfo { get; set; }
+      internal object Traget { get; private set; }
+
+      internal static OnNextInvoker Create<T>(Subject<T> subject)
+      {
+        return new OnNextInvoker
+        {
+          Traget = subject,
+          MethodInfo = typeof(Subject<T>).GetMethod(nameof(subject.OnNext))
+        };
+      }
+
+      internal void Invoke(object input)
+      {
+        MethodInfo.Invoke(Traget, new[] { input });
+      }
+    }
+  }
+}
